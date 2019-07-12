28033f4f3250945f3e2c68677a2b968f375a703f
Johannes Lantz <lithvia@gmail.com>
2019-07-12 09:46:58 +0200

    Controlling the Flow of Execution

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -11,9 +11,45 @@ namespace GradeBook
             Name = name;
         }
 
+        public void AddLetterGrade(char letter)
+        {
+            switch(letter)
+            {
+                case 'A':
+                    AddGrade(90);
+                    break;
+                case 'B':
+                    AddGrade(80);
+                    break;
+                case 'C':
+                    AddGrade(70);
+                    break;
+                case 'D':
+                    AddGrade(60);
+                    break;
+                case 'E':
+                    AddGrade(50);
+                    break;
+                case 'F':
+                    AddGrade(40);
+                    break;
+                default:
+                    AddGrade(0);
+                    break;
+            }
+
+        }
+
         public void AddGrade(double grade)
         {
-          grades.Add(grade);
+            if(grade <= 100 && grade >= 0)
+            {
+                grades.Add(grade);
+            }
+            else
+            {
+                throw new ArgumentException($"Invalid {nameof(grade)}");
+            }
         }
         
         public Statistics GetStatistics()
@@ -23,14 +59,39 @@ namespace GradeBook
             result.High = double.MinValue;
             result.Low = double.MaxValue;
 
-            foreach(double grade in grades)
+            for(var index = 0; index < grades.Count; index++)
             {
-                result.High = Math.Max(grade, result.High);
-                result.Low = Math.Min(grade, result.Low);
-                result.Average += grade;
+                if(grades[index] == 42.1){
+                    continue;
+                }
+
+                result.High = Math.Max(grades[index], result.High);
+                result.Low = Math.Min(grades[index], result.Low);
+                result.Average += grades[index];
+                index += 1;
             }
 
             result.Average /= grades.Count;
+
+
+            switch(result.Average)
+            {
+                case var d when d > 90.0:
+                    result.Letter = 'A';
+                    break;
+                case var d when d > 80.0:
+                    result.Letter = 'B';
+                    break;
+                case var d when d > 70.0:
+                    result.Letter = 'C';
+                    break;
+                case var d when d > 60.0:
+                    result.Letter = 'D';
+                    break;
+                default:
+                    result.Letter = 'F';
+                    break;
+            }
 
             return result;
         }
--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -8,14 +8,42 @@ namespace GradeBook
         static void Main(string[] args)
         {
             var book = new Book("Grade Book");
-            book.AddGrade(89.1);
-            book.AddGrade(98.1);
-            book.AddGrade(77.4);
+
+
+            while(true)
+            {
+                Console.WriteLine("Enter a grade ir enter 'q' to quit: ");
+                var input = Console.ReadLine();
+
+                if(input == "q"){
+                    break;
+                }
+
+                try
+                {
+                    var grade = double.Parse(input);
+                    book.AddGrade(grade);
+                }
+                catch(ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                catch(FormatException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    Console.WriteLine("**");
+                }
+            }
+
             var stats = book.GetStatistics();
 
             Console.WriteLine($"The highest grade is {stats.High:N1}");
             Console.WriteLine($"The average grade is {stats.Average:N1}");
             Console.WriteLine($"The lowest grade is {stats.Low:N1}");
+            Console.WriteLine($"The letter grade us {stats.Letter}");
         }
     }
 }
